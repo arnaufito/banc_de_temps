3c0687eb21e6aa5db3e70d00616e5eba7b30bbd3
Arnau Fitó Ruana <3966305095@edu.gencat.cat>
2026-07-20 18:19:09 +0200

    prova de la pujsada al servir de la ruta del ofertes

--- app.py
+++ app.py
@@ -192,9 +192,10 @@ def historial():
 # ==========================================
 if __name__ == "__main__":
     app.run(debug=True)
-
+#prova
 @app.route("/oferta/<int:id_oferta>")
 def detall_oferta(id_oferta):
+    
     conn = sqlite3.connect("banc_temps.db")
     cursor = conn.cursor()
     cursor.execute("SELECT id, titol, descripcio, hores FROM ofertes WHERE id = ?", (id_oferta,))
