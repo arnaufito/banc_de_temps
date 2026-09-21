bf3ef0432e9eadc7ab9b05aea648917149a73542
Arnau Fitó <afitoruana@gmail.com>
2026-09-21 13:55:48 +0200

    nou codi de dmin, al principi de def inicialitzar, i nou codi de login, crec que es exactament el mateix qaue l ultim cop pero bueno

--- app.py
+++ app.py
@@ -85,12 +85,13 @@ def inicialitzar_bd():
         missatge TEXT NOT NULL
     )
     ''')
-    # TRUC PER AFEGIR EL ROL D'ADMINISTRADOR SENSE TRENCAR LA BD
+    # TRUC PER ACTUALITZAR LA TAULA USUARIS
     try:
         cursor.execute("ALTER TABLE usuaris ADD COLUMN es_admin INTEGER DEFAULT 0")
     except sqlite3.OperationalError:
-        pass # Si la columna ja hi és, continua endavant
-
+        pass # Si la columna ja existeix, l'ignora i continua
+        
+    
     conn.commit()
     conn.close()
 
@@ -153,22 +154,26 @@ def login():
         conn = sqlite3.connect("banc_temps.db")
         cursor = conn.cursor()
         
-        # ARA TAMBÉ DEMANEM LA COLUMNA 'es_admin'
-        cursor.execute("SELECT id, nom, contrasenya, es_admin FROM usuaris WHERE correu = ?", (correu_usuari,))
+        # 1. ARA LI DEMANEM TAMBÉ EL CAMP 'es_admin'
+        try:
+            cursor.execute("SELECT id, nom, contrasenya, es_admin FROM usuaris WHERE correu = ?", (correu_usuari,))
+        except sqlite3.OperationalError:
+            # Si per algun motiu la columna encara no existeix, t'assigna un 0 per defecte perquè no falli
+            cursor.execute("SELECT id, nom, contrasenya, 0 as es_admin FROM usuaris WHERE correu = ?", (correu_usuari,))
+            
         usuari = cursor.fetchone()
         conn.close()
         
         if usuari and check_password_hash(usuari[2], contrasenya_usuari):
+            # 2. GUARDEM LES DADES A LA TEVA SESSIÓ
             session['id_usuari'] = usuari[0]
             session['nom'] = usuari[1]
-            session['es_admin'] = usuari[3] # Guardem si és administrador (1) o no (0)
+            session['es_admin'] = usuari[3]  # Això és el que et dona el "poder" dins la web!
             return redirect(url_for('mercat'))
         else:
-            # Si falla, mostrem un error
             return "<h3>Correu o contrasenya incorrectes.</h3><br><a href='/login'>Torna-ho a provar</a>"
             
     return render_template("login.html")
-
 @app.route("/admin")
 def admin():
     # BARRERA DE SEGURETAT: Si no està loguejat o no és admin, fora.
